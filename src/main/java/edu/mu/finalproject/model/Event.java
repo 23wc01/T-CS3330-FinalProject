10aa73560233d1bee6be47064e2120adb9ae5e1e
evieteru <eviezushi@Nod>
2024-04-17 15:33:19 -0500

    Added Event class

--- src/main/java/edu/mu/finalproject/model/Event.java
+++ src/main/java/edu/mu/finalproject/model/Event.java
@@ -1,5 +1,43 @@
 package edu.mu.finalproject.model;
 
-public class Event {
+import java.time.MonthDay;
+import java.time.Year;
 
+public class Event {
+	
+	protected MonthDay eventMonthDay;
+	protected Year eventYear;
+	
+	protected String artistName;
+	
+	
+
+	public MonthDay getEventMonthDay() {
+		return eventMonthDay;
+	}
+	public void setEventMonthDay(MonthDay eventMonthDay) {
+		this.eventMonthDay = eventMonthDay;
+	}
+	
+	
+
+	public Year getEventYear() {
+		return eventYear;
+	}
+	public void setEventYear(Year eventYear) {
+		this.eventYear = eventYear;
+	}
+	
+	
+
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+
+		
+		
+		
 }
