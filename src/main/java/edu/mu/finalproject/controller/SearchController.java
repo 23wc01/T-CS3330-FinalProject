2b4d892619a13b7f002755cde5925be00debb3fb
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-18 17:22:05 -0700

    Modified SearchController.java - Got rid of statement to save results as string

--- src/main/java/edu/mu/finalproject/controller/SearchController.java
+++ src/main/java/edu/mu/finalproject/controller/SearchController.java
@@ -10,10 +10,6 @@ public class SearchController {
 	public void searchSort(String queryString, ArrayList<MediaObject> catalog) {
 		SearchComparator songSearch = new SearchComparator(queryString);
 		Collections.sort(catalog, songSearch);
-		String resultsList = "Search results for '"+ queryString + "'\n";
-		for (MediaObject mediaObj : catalog) {
-			resultsList += mediaObj.toString() + "\n";
-		}
 		SearchView searchResultsView = new SearchView();
 		searchResultsView.DisplaySearchResultsView(queryString, catalog);
 		return;
