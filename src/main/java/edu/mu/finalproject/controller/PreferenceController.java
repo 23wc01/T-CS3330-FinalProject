0372600751ad18a552e99b72fb1e1061f9b55c9c
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-19 07:54:10 -0700

    Added PreferenceController. Modified Preference.java to include multiple values for enum constants

---
+++ src/main/java/edu/mu/finalproject/controller/PreferenceController.java
@@ -0,0 +1,11 @@
+package edu.mu.finalproject.controller;
+
+import edu.mu.finalproject.model.Preference;
+
+public class PreferenceController {
+	public void setupPreferences() {
+		// Need to have Account user as argument in setupPreferences
+		Preference userPreference = Preference.BASS;
+		
+	}
+}
