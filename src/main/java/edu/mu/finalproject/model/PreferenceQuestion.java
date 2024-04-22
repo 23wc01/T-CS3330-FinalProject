fe6ddc857b5deb9815ac747d1679c2d4d1fe34ae
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-21 20:35:35 -0700

    Updated PreferenceController.java - Added implementation for actually reading questions & scoring answers base off of data from json file using gson dependency

--- src/main/java/edu/mu/finalproject/model/PreferenceQuestion.java
+++ src/main/java/edu/mu/finalproject/model/PreferenceQuestion.java
@@ -2,15 +2,17 @@ package edu.mu.finalproject.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class PreferenceQuestion {
 	private String question;
-	private HashMap<String, Preference> choicesToPreferences = new HashMap<String, Preference>();
-	
-	public PreferenceQuestion(String question, HashMap<String, Preference> choicesToPreferences) {
+	private ArrayList<HashMap<String, String>> choicesToPrefs;
+	public PreferenceQuestion() {}
+	public PreferenceQuestion(String question, ArrayList<HashMap<String, String>> choicesToPrefs) {
 		super();
 		this.question = question;
-		this.choicesToPreferences = choicesToPreferences;
+		this.choicesToPrefs = choicesToPrefs;
 	}
 	public String getQuestion() {
 		return question;
@@ -19,10 +21,24 @@ public class PreferenceQuestion {
 		this.question = question;
 	}
 
-	public HashMap<String, Preference> getChoiceToScoring() {
-		return choicesToPreferences;
+	public ArrayList<HashMap<String, String>> getChoiceToPreferences() {
+		return choicesToPrefs;
 	}
-	public void setChoiceToScoring(HashMap<String, Preference> choicesToPreferences) {
-		this.choicesToPreferences = choicesToPreferences;
+	public void setChoiceToPreferences(ArrayList<HashMap<String, String>> choicesToPrefs) {
+		this.choicesToPrefs = choicesToPrefs;
 	}
-}
+	public ArrayList<String> getChoices() {
+		ArrayList<String> choices = new ArrayList<String>();
+		for(Map<String, String> choiceToPreferences : choicesToPrefs) {
+			choices.add(choiceToPreferences.get("choice").toString());
+		}
+		return choices;
+	}
+	public ArrayList<String> getAnswerPreferences() {
+		ArrayList<String> answerPreferences = new ArrayList<String>();
+		for(Map<String, String> choiceToPreferences : choicesToPrefs) {
+			answerPreferences.add(choiceToPreferences.get("preference").toString());
+		}
+		return answerPreferences;
+	}
+ }
--- src/main/java/edu/mu/finalproject/view/SetupPreferenceView.java
+++ src/main/java/edu/mu/finalproject/view/SetupPreferenceView.java
@@ -1,12 +1,23 @@
 package edu.mu.finalproject.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import edu.mu.finalproject.util.GetIntegerInput;
+
 public class SetupPreferenceView {
-	public void displayQuestion(String question) {
-		System.out.println(question);
+	public void displaySetupIntro() {
+		System.out.println();
+		System.out.println("This is a multiple choice quiz to help determine your preference as a listener.");
+		System.out.println("To answer each question, enter a number (1~6) that corresponds with your answer choice.");
+		System.out.println("*******************************");
 	}
-	public void displayChoices(String[] choices) {
+	public void displayQuestion(String question) {
+		System.out.println();
+		System.out.println(question);
+		System.out.println("-------------------------------");
+	}
+	public void displayChoices(ArrayList<String> choices) {
 		int choiceNum = 1;
 		for (String choice : choices) {
 			System.out.println(choiceNum + " - " + choice);
@@ -14,9 +25,18 @@ public class SetupPreferenceView {
 		}
 	}
 	public int getInputAnswer() {
-		System.out.print("Enter choice's number: ");
-		Scanner scanner = new Scanner(System.in);
-		int answer = scanner.nextInt();
+		System.out.print("\nInput MUST be between (1~6). ");
+		int answer = GetIntegerInput.getInputInteger(new Scanner(System.in));
+		while(answer < 1 || answer > 6) {
+			System.out.println("Enter a number between 1~6!!!\n");
+			answer = getInputAnswer();
+		}
 		return answer;
+	}
+	public void displayPreference(String preference) {
+		System.out.println("\n Results:");
+		System.out.println("You're a(n) " + preference + " lover!");
+		System.out.println("*******************************");
+		System.out.println();
 	}
 }
