183bf06d58e57ef6b8622b913ecb2b5561e08cbf
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-24 08:23:03 -0500

    Updated SetupPreferenceView - Made some more formatting changes to SetupPreferenceView

--- src/main/java/edu/mu/finalproject/view/SetupPreferenceView.java
+++ src/main/java/edu/mu/finalproject/view/SetupPreferenceView.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
 import edu.mu.finalproject.util.GetIntegerInput;
 
 public class SetupPreferenceView {
+	
 	public void displaySetupIntro() {
 		System.out.println();
 		System.out.println("Here's a multiple choice quiz to help determine your preference as a listener!");
 		System.out.println("*******************************");
 	}
+	
 	public void displayQuestion(String question) {
 		System.out.println();
 		System.out.println(question);
 		System.out.println("-------------------------------");
 	}
+	
 	public void displayChoices(ArrayList<String> choices) {
 		int choiceNum = 1;
 		for (String choice : choices) {
@@ -23,6 +26,7 @@ public class SetupPreferenceView {
 			++choiceNum;
 		}
 	}
+	
 	public int getInputAnswer() {
 		System.out.print("\nInput MUST be between (1~6). ");
 		int answer = GetIntegerInput.getInputInteger(new Scanner(System.in));
@@ -32,6 +36,7 @@ public class SetupPreferenceView {
 		}
 		return answer;
 	}
+	
 	public void displayPreference(String preference) {
 		System.out.println("\nResults:");
 		System.out.println("You're a(n) " + preference + " lover!");
@@ -39,3 +44,4 @@ public class SetupPreferenceView {
 		System.out.println();
 	}
 }
+ 
