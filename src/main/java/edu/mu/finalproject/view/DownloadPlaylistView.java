e9724efa0d18a9ba1e6d16d30ace18531843cdb9
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-04-27 13:45:39 -0500

    Modified views so that they return boolean

--- src/main/java/edu/mu/finalproject/view/DownloadPlaylistView.java
+++ src/main/java/edu/mu/finalproject/view/DownloadPlaylistView.java
@@ -1,7 +1,17 @@
 package edu.mu.finalproject.view;
 
 public class DownloadPlaylistView {
+	/**
+	 * Displays playlist being downloaded to @param outputFilepath. Returns false if parameters were null or if writeIsSuccess was false
+	 * @param outputFilepath
+	 * @param writeIsSuccess
+	 * @return
+	 */
 	public Boolean displayDownloadMessage(String outputFilepath, Boolean writeIsSuccess) {
+		if (outputFilepath == null || writeIsSuccess == null) {
+			System.out.println("Output file path or writeIsSuccess was uninitialized.");
+			return false;
+		}
 		if (writeIsSuccess) {
 			System.out.println("Saved playlist's html file to '" + outputFilepath + "'");
 			return true;
@@ -10,6 +20,5 @@ public class DownloadPlaylistView {
 			System.out.println("Error in writing to '" + outputFilepath + "'");
 			return false;
 		}
-		
 	}
 }
