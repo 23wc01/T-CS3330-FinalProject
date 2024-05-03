82671de3941970c78df38a8e110274e2031f7819
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2024-05-02 22:00:37 -0500

    Modified CatalogController - Simplified some of the implementation. Still needs to displayShuffle

--- src/main/java/edu/mu/finalproject/Main.java
+++ src/main/java/edu/mu/finalproject/Main.java
@@ -1,7 +1,5 @@
 package edu.mu.finalproject;
 
-
-
 public class Main {
 
 	public static void main(String[] args) {
--- src/main/java/edu/mu/finalproject/controller/MediaCatalogController.java
+++ src/main/java/edu/mu/finalproject/controller/MediaCatalogController.java
@@ -6,47 +6,48 @@ import edu.mu.finalproject.model.MediaProduct;
 import edu.mu.finalproject.model.Playlist;
 import edu.mu.finalproject.model.Song;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MediaCatalogController {
     private final CatalogSingleton catalogSingleton;
+    private final ArrayList<MediaProduct> catalogSingleton_Collection;
     private final Random random = new Random();
 
     public MediaCatalogController() {
         catalogSingleton = CatalogSingleton.getInstance();
+        catalogSingleton_Collection = catalogSingleton.getMediaProductCollection();
     }
 
     public void addMedia(MediaProduct mediaProduct) {
-    	
-        catalogSingleton.addMediaProduct(mediaProduct);
+    	catalogSingleton_Collection.add(mediaProduct);
     }
 
     public void deleteMedia(int id) {
-        catalogSingleton.removeMediaProduct(id);
+    	catalogSingleton_Collection.remove(id);
     }
 
     public void displayAll() {
-        catalogSingleton.displayAll();
+        for (MediaProduct mediaProduct : catalogSingleton_Collection) {
+        	System.out.println(mediaProduct);
+        }
     }
 
     public void displayShuffle() {
-        catalogSingleton.displayShuffle();
+    	catalogSingleton_Collection.displayShuffle();
     }
 
     public void loadMediaFromFiles() {
         List<Song> songs = MediaFileReader.readSongs();
         List<Playlist> playlists = MediaFileReader.readPlaylists();
-        songs.forEach(catalogSingleton::addMediaProduct);
-        playlists.forEach(catalogSingleton::addMediaProduct);
-    }
-
-    public void findAndDisplaySongs(String songName) {
-        List<MediaProduct> results = catalogSingleton.searchMedia(songName);
-        if (!results.isEmpty()) {
-            results.forEach(System.out::println);
-        } else {
-            System.out.println("No songs found matching: " + songName);
+        for (Song song : songs) {
+        	addMedia(song);
+        }
+        for (Playlist playlist : playlists) {
+        	addMedia(playlist);
         }
     }
+
+ 
 }
